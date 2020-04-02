8c59f46de8ab49050f6d2536abd9b909d6bfc276
samgos <samuel.jj.gosling@gmail.com>
2020-04-02 15:32:10 +0100

    proposal structure groundwork

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -9,22 +9,67 @@ contract DAO {
     uint qualityRep;
   }
 
+  struct Proposal {
+    mapping (address => bool) endorsers;
+    unit endorsementCount;
+    bool approvalState;
+    bytes32 ipfsHash;
+    uint bidAmount;
+  }
+
+  mapping (address => Proposal) public proposals;
   mapping (address => Member) public committee;
 
   address[] public members;
 
   constructor() addCommitteeMember(msg.sender) public { }
 
-  modifier _isCommitteeMember(address acc, bool state){
+  modifier _isCommitteeMember(address acc, bool state) {
     if(state) require(committee[acc].blockNumber != 0)
     else require(committee[acc].blockNumber == 0)
     _;
   }
 
+  modifier _isValidProposal(string subject) {
+    require(proposal[subject].ipfsHash != bytes32(0x0));
+    require(proposal[subject].endorsementCount != 0);
+    _;
+  }
+
+  modifier _isVerifiedUser(address subject) { }
+
   function addCommitteeMember(address individual)
-    _isCommitteeMember(individual, false) private {
+    _isCommitteeMember(individual, false)
+  private {
     committee[individual].blockNumber = block.number
     members.push(individual)
+  }
+
+  function createProposal(string subject, bytes32 ipfsHash)
+  public payable {
+    require(ipfsHash != bytes(0x0) && subject.length != 0)
+    require(proposal[subject].ipfsHash == bytes32(0x0))
+
+    proposal[subject].bidAmount = msg.value;
+    proposal[subject].ipfsHash = ipfsHash;
+  }
+
+  function fundProposal(string subject)
+    _isValidProposal(subject)
+  public payable {
+    require(msg.value > 1 wei);
+
+    uint existingBid = proposal[subject].bidAmount;
+    proposal[subject].bidAmount = existingBid.add(msg.value);
+  }
+
+  function endorseProposal(string subject)
+    _isValidProposal(subject) _isVerifiedUser(msg.sender)
+  public {
+    require(!proposal[subject].endorsers[msg.sender])
+    
+    proposal[subject].endorsers[msg.sender] = true;
+    proposal[subject].endorsementCount++;
   }
 
 }
