11e3997a7f2cc91b9b44897c1564452142da7477
samgos <samuel.jj.gosling@gmail.com>
2020-04-02 19:17:12 +0100

    proposal approval basis

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -6,10 +6,22 @@ contract DAO {
 
   using SafeMath for uint;
 
+  bytes32 constant POS = 0x506f736974697665000000000000000000000000000000000000000000000000;
+  bytes32 constant NEU = 0x4e65757472616c00000000000000000000000000000000000000000000000000;
+
+  struct Approval {
+    mapping (address => bytes32) accreditors;
+    address[] participants;
+    uint expirationDate;
+    uint negativeCount;
+    uint positiveCount;
+    address proposee;
+  }
+
   struct Proposal {
     mapping (address => bool) endorsers;
-    uint endorsementCount;
     bool approvalState;
+    uint endorsements;
     bool queryState;
     bytes ipfsHash;
     uint bidAmount;
@@ -23,6 +35,7 @@ contract DAO {
   }
 
   mapping (string => Proposal) public proposals;
+  mapping (string => Approval) public approvals;
   mapping (address => Member) public committee;
 
   address[] public members;
@@ -43,7 +56,20 @@ contract DAO {
     _;
   }
 
+  modifier _canVote(string memory subject) {
+    require(approvals[subject].accreditors[msg.sender] == bytes(0x0));
+    require(approvals[subject].expirationDate >= block.timestamp);
+    require(!proposals[subject].approvalState);
+    require(proposals[subject].queryState);
+    _;
+  }
+
   modifier _isVerifiedUser(address account) { _; }
+
+  function getQuorum() public view returns (uint) {
+    if(members.length % 2 == 0) return members.length.div(2);
+    else return members.length.sub(1).div(2);
+  }
 
   function addCommitteeMember(address individual)
     _isCommitteeMember(individual, false)
@@ -57,7 +83,46 @@ contract DAO {
   public {
     require(!proposals[subject].approvalState);
     require(!proposals[subject].queryState);
+
+    uint expiryTimestamp = block.timestamp + 604800;
+
+    approvals[subject].expirationDate = expiryTimestamp;
+    approvals[subject].proposee = msg.sender;
+    proposals[subject].queryState = true;
   }
+
+  function voteApproval(string memory _subject, bytes32 _choice)
+    _isCommitteeMember(msg.sender, true) _canVote(subject)
+  public {
+    require(_choice == NEG || _choice == POS);
+
+    approvals[subject].accreditors[msg.sender] = _choice;
+    approvals[subject].participants.push(msg.sender);
+  }
+
+  function concludeApproval(string memory _subject)
+    _isCommitteeMember(msg.sender)
+  public {
+    require(approvals[subject].expirationDate < block.timestamp);
+
+    for(var x = 0 ; x < approvals[subject].participants ; x++) {
+      var focusPoint = approvals[subject].participants[x];
+      var decision = approvals[subject].accreditors[focusPoint];
+
+      if(decision == POS) approvals[subject].positiveCount++;
+      else if(decision == NEG) approvals[subject].negativeCount++;
+    }
+
+    var confirmations = approvals[subject].positiveCount;
+    var rejections = approvals[subject].negativeCount;
+
+    if(approvals => rejections) executeProposal(subject)
+
+    emit Approval(subject, confirmations, rejections);
+    delete approvals[subject];
+  }
+
+  function executeProposal(string memory subject) { }
 
   function createProposal(string memory subject, bytes memory ipfsHash)
   public payable {
@@ -72,6 +137,7 @@ contract DAO {
     _isValidProposal(subject)
   public payable {
     uint existingBid = proposals[subject].bidAmount;
+
     proposals[subject].bidAmount = existingBid.add(msg.value);
   }
 
