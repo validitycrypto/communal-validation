7a2ded99c49052df543cd1206d76dea39cd28694
samgos <samuel.jj.gosling@gmail.com>
2020-04-02 15:08:35 +0100

    basic committee member onboarding

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -1,3 +1,30 @@
 pragma solidity ^0.6.4;
 
-contract DAO { }
+contract DAO {
+
+  struct Member {
+    uint blockNumber;
+    uint proposalCount;
+    uint reportCount;
+    uint qualityRep;
+  }
+
+  mapping (address => Member) public committee;
+
+  address[] public members;
+
+  constructor() addCommitteeMember(msg.sender) public { }
+
+  modifier _isCommitteeMember(address acc, bool state){
+    if(state) require(committee[acc].blockNumber != 0)
+    else require(committee[acc].blockNumber == 0)
+    _;
+  }
+
+  function addCommitteeMember(address individual)
+    _isCommitteeMember(individual, false) private {
+    committee[individual].blockNumber = block.number
+    members.push(individual)
+  }
+
+}
